17956eb4302c9ed682d0b3e6a59e69fa6d09df33
viktorisup <101949774+viktorisup@users.noreply.github.com>
2024-10-28 13:17:06 +0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BACKUP_DIR="/opt/database/backup"
+TIMESTAMP=$(date +\%Y-\%m-\%d)
+BACKUP_FILE="$BACKUP_DIR/txn_info_$TIMESTAMP.tar"
+ENCRYPTED_FILE="$BACKUP_FILE.enc"
+KEY="mykey" 
+BUCKET="s3://xxxxx"
+
+pg_dump -F t -h localhost -U psp_kg_user -d txn_info > "$BACKUP_FILE"
+openssl enc -aes-256-cbc -salt -pbkdf2 -in "$BACKUP_FILE" -out "$ENCRYPTED_FILE" -k "$KEY"
+rm "$BACKUP_FILE"
+s3cmd put "$ENCRYPTED_FILE" "$BUCKET" 
+find "$BACKUP_DIR" -type f -name "*.tar.enc" -mtime +2 -exec rm {} \;
